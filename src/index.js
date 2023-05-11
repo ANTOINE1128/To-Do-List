70b8d017d4c47a8109a7f41bf2b77c690a332175
ANTOINE1128 <makdessyantoine@gmail.com>
2023-05-11 21:32:37 +0300

    Add Remove btns

--- src/index.js
+++ src/index.js
@@ -1,39 +1,36 @@
 import './style.css';
-const toDoTasks = [
-  {
-    description: 'task one',
-    completed: false,
-    index: 0,
-  },
-  {
-    description: 'task two',
-    completed: true,
-    index: 1,
-  },
-  {
-    description: 'task tree',
-    completed: false,
-    index: 2,
-  },
-];
+import UI from './modules/UI.js';
+import Store from './modules/localstorage.js';
 
-// defining the UL id
-const listItem = document.getElementById('list-item');
+// Event for Displaying the to do tasks
+document.addEventListener('DOMContentLoaded', UI.displayToDoList);
 
-// loop through the array of obj and create li for each
-toDoTasks.forEach((todo) => {
-  // Create a new li element
-  const listElement = document.createElement('li');
-  const spanElement = document.createElement('span');
-  spanElement.textContent = todo.description;
-
-  const checkboxElement = document.createElement('input');
-  checkboxElement.type = 'checkbox';
-  listElement.appendChild(checkboxElement);
-  listElement.appendChild(spanElement);
-
-  if (todo.completed) {
-    listItem.classList.add('completed');
+// Event for adding new to do tasks
+document.querySelector('#todo-form').addEventListener('keypress', (e) => {
+  if (e.key === 'Enter') {
+    e.preventDefault();
+    UI.getNewToDoTask();
   }
-  listItem.appendChild(listElement);
+});
+
+// Event for deleting new item for the list
+document.querySelector('#list-item').addEventListener('click', (e) => {
+  Store.removetodoTask(e.target);
+  UI.deletetodoTask(e.target);
+});
+
+// Event for editing the to do list
+document.querySelector('#list-item').addEventListener('click', (e) => {
+  if (e.target.classList.contains('bi-pencil-square')) {
+    UI.editeToDoTask(e.target);
+  }
+});
+
+// Event for updating tasks
+document.querySelector('#list-item').addEventListener('keypress', (e) => {
+  if (e.key === 'Enter' && e.target.classList.contains('edit-list')) {
+    e.preventDefault();
+    Store.updatetodoTask(e.target);
+    window.location.reload();
+  }
 });
